ed7844ac0770d1fdf01c38160b426f3c8241e439
Manuel <manuelnuez@tutanota.com>
2025-01-27 18:34:14 -0300

    Try new strategy for render on auth

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const SECRET_KEY = process.env.SECRET_KEY ?? ''
+const SECRET_KEY = process.env.SECRET_KEY ?? 'secret'
 
 const validateInput = (email?: string, username?: string, password?: string): boolean => {
   return !!(email ?? username) && !!password;
