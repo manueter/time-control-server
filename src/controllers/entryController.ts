3d81ed33f4dfcb2feb82eebe33338cfcf50faec6
Manuel <manuelnuez@tutanota.com>
2025-02-04 22:16:38 -0300

    Change Date format at endpoints now expects anr returns Date string as dd/mm/yyyy

--- src/controllers/entryController.ts
+++ src/controllers/entryController.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
 import { addEntry, getEntriesForUser } from "../models/entryModel";
+import { formatDatePostgres, formatDateToStringDDMMYYYY, isValidDate } from "../utils/dateUtils";
+
+interface GetEntriesQuery {
+  user_uuid: string;
+  start_date?: string;
+  end_date?: string;
+}
 
 export const createEntry = async (req: Request, res: Response): Promise<void> => {
   const { user_uuid, entry_type_id, clock_id } = req.body;
@@ -13,22 +20,36 @@ export const createEntry = async (req: Request, res: Response): Promise<void> =>
 };
 
 export const getEntries = async (req: Request, res: Response): Promise<void> => {
-  const { user_uuid, start_date, end_date } = req.query;
+  // const { user_uuid, start_date as string, end_date } = req.query;
+  const user_uuid = req.query.user_uuid as string;
+  const start_date = req.query.start_date as string;
+  const end_date = req.query.end_date as string;
 
   if (!user_uuid) {
     res.status(400).json({ message: "user_uuid is required" });
     return;
   }
 
-  try {
-    const entries = await getEntriesForUser(
-      user_uuid as string,
-      start_date as string,
-      end_date as string
-    );
-    res.status(200).json(entries);
-  } catch (error) {
-    console.error("Error fetching entries:", error);
-    res.status(500).json({ message: "Failed to fetch entries" });
+  if (start_date && end_date) {
+    if (!isValidDate(start_date) || !isValidDate(end_date)) {
+      throw new Error("Invalid date format. Expected DD/MM/YYYY.");
+    }
+    try {
+      const entries = await getEntriesForUser(
+        user_uuid,
+        formatDatePostgres(start_date),
+        formatDatePostgres(end_date)
+      );
+      const formattedEntries = entries.map(entry => ({
+        ...entry,
+        date: formatDateToStringDDMMYYYY(entry.date),
+      }));
+      res.status(200).json(formattedEntries);
+    } catch (error) {
+      console.error("Error fetching entries:", error);
+      res.status(500).json({ message: "Failed to fetch entries" });
+    }
   }
+
+  
 };
--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -13,3 +13,27 @@ const localTimeZone = 'America/Argentina/Buenos_Aires';
     const localDate = toZonedTime(date, localTimeZone);
     return format(localDate, 'yyyy-MM-dd HH:mm:ss', { timeZone: localTimeZone });
   };
+
+  export const formatDateToStringDDMMYYYY = (date: Date): string => {
+
+    const yyyy = date.getFullYear();
+    const month = date.getMonth() + 1; // Months start at 0!
+    const day = date.getDate();
+
+    let mm=`${month}`
+    let dd=`${day}`
+    if(month<10){mm='0'+mm}
+    if(day<10){dd='0'+dd}
+
+    return `${dd}/${mm}/${yyyy}`;
+  };
+
+  export const isValidDate = (date: string): boolean => {
+    const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+    return regex.test(date);
+  };
+  
+  export const formatDatePostgres = (date: string): string => {
+    const [day, month, year] = date.split("/");
+    return `${year}/${month}/${day}`;
+  };
